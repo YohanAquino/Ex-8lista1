e31bd94520711c4901a05f36cda8d29fcbd69307
YohanAquino <74798062+YohanAquino@users.noreply.github.com>
2020-11-27 21:38:15 -0300

    Add files via upload

---
+++ EX-8-LISTA-1-Yohan_Aquino/EX-8-LISTA-1-Yohan_Aquino/Comparador.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_8_LISTA_1_Yohan_Aquino
+{
+    class Comparador
+    {
+        private double n1;
+        private double n2;
+
+        public void setNum1(double x)
+        {
+            n1 = x;
+        }
+        public void setNum2(double x)
+        {
+            n2 = x;
+        }
+        public string getMaior()
+        {
+            if (n1 == n2)
+            {
+                return "os valores são identicos";
+            }
+            else
+            {
+                if (n1 > n2)
+                {
+                    return n1.ToString()+" é o maior";
+                }
+                else
+                {
+                    return n2.ToString()+" é o maior";
+                }
+            }
+        }
+    }
+}
---
+++ EX-8-LISTA-1-Yohan_Aquino/EX-8-LISTA-1-Yohan_Aquino/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_8_LISTA_1_Yohan_Aquino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*9. Entrar com dois valores quaisquer. Exibir o maior deles, se existir, caso contrário, enviar
+mensagem avisando que os números são idênticos. */
+            Comparador com = new Comparador();
+
+            Console.WriteLine("Digite o 1° valor");
+            com.setNum1(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite o 2° valor");
+            com.setNum2(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("{0}", com.getMaior());
+            Console.ReadKey();
+        }
+    }
+}
